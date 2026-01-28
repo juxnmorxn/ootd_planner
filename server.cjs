2bb71f7adfec69c2acde2bdce27d569fbffc255f
juxmorxn <juanmonan06@gmail.com>
2026-01-28 12:54:04 -0600

    Fix: Express wildcard route syntax for production

--- server.cjs
+++ server.cjs
@@ -694,7 +694,7 @@ app.get('/api/stats/:userId', (req, res) => {
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Catch-all route to serve the SPA (for React Router/Navigation)
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     if (!req.path.startsWith('/api')) {
         res.sendFile(path.join(__dirname, 'dist', 'index.html'));
     }
